d7913afc7f3fc4e3daa261d5439a8592eed65e6f
sy-md <mdorsetti733@gmail.com>
2023-04-08 00:58:05 -0700

    finshed my plugin for logseq

--- atna_log.py
+++ atna_log.py
@@ -1,35 +1,70 @@
 import os
 import time
 import shutil
+import click
 import logging as lg
 
 block = "\n- "
 journal = "journals"
 page = "pages"
 
-# the only thing left is to make a fuzzy finder for the files and know file i want to add to
+# create a logger
+lg.basicConfig(filename="atna_log.log", level=lg.INFO)
 
-def journal_entry():
-    t = os.listdir(journal)
-    my_file = os.path.join(journal,t[-1])
-    print(my_file)
-    with open( my_file ,"a") as thought:
-        print("write your thought:")
-        #input_string =("{}first test from the script with athena help [[chatGPT]]".format(block))
-        input_string = input("{}".format(block))
-        thought.write(input_string)
-    time.sleep(3)
-    return my_journal_refs(input_string)
+def log_seq_app():
+    # log that the app was started
+    lg.info("app started")
+    def my_journal_refs():
+        find_journal = os.listdir(journal)
+        last_journal = find_journal[-1]
+        # log that the journal was found and the name of the journal
+        lg.info("journal found: " + last_journal)
+        path_to_journal = os.path.join(journal, last_journal)
+        # log that the journal was found
+        lg.info("journal found")
+        with open(path_to_journal, "a") as f: # make sure to change this to "w" when you are done testing
+            # log that the user is adding a journal entry to name of journal
+            lg.info("adding journal entry to: " + last_journal)
+            input_string = click.prompt("Add a journal entry")
+            f.write(block + input_string)
+            time.sleep(3)
+            if input_string.find("[[") == -1:
+                question = click.prompt("Did you forget to add a page entry? y/n")
+                if question == "y":
+                    add_page = click.prompt("Add a page entry")
+                    my_page_refs(input_string, forgot_brackets=add_page)
+                elif question == "n":
+                    quit()
+                else:
+                    print("Invalid input")
 
-def my_journal_refs(input_string): # the jounal string
-    start_index = input_string.index("[[") + 2
-    end_index = input_string.index("]]")
-    word = input_string[start_index:end_index]
-    my_file = os.path.join(page,word + ".md")
-    with open(my_file ,"w") as file:
-        print("write a note:")
-        x = input("{}".format(block))
-        file.write(x)
-    print(f"{word}.md created successfully!")
+            return my_page_refs(input_string)
 
-journal_entry()
+        
+    def my_page_refs(input_string, forgot_brackets=""):
+        if forgot_brackets != "": # if the user forgot to add brackets
+            input_string = forgot_brackets
+
+        find_page = input_string.find("[[") + 2 
+        find_page2 = input_string.find("]]") 
+        word = input_string[find_page:find_page2] 
+        my_file = os.path.join(page, word + ".md")
+        # log that the page was created
+        lg.info("page created")
+        with open(my_file, "w") as f:
+            input_string = click.prompt("Add a page entry: ")
+            f.write(block + input_string)
+            lg.info("page entry added")
+            time.sleep(3)
+
+    user = click.prompt("press 1 to add a journal entry or 2 to add a page entry")
+    if user == "1":
+        click.echo("use [[ ]] to add a page entry {for example [[page name]]}")
+        my_journal_refs()
+    elif user == "2":
+        my_page_refs()
+    else:
+        print("Invalid input")
+
+if __name__ == "__main__":
+    log_seq_app()   
